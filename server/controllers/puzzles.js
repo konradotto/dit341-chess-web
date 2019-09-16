d4c1e106a934392fd62bb9e26e2a8b7ec5c5c733
konradotto <konrad-otto@gmx.de>
2019-09-16 02:19:09 +0200

    Add basic routing functionality to controllers/puzzles.js

--- server/controllers/puzzles.js
+++ server/controllers/puzzles.js
@@ -3,3 +3,9 @@
  * The purpose of this module is to define the RESTful API 
  * for accessing stored puzzles. 
  */
+
+let express = require('express');
+let router = express.Router();
+let Puzzle = require('../models/puzzle');
+
+module.exports = router;
