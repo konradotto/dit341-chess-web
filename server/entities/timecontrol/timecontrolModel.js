28cd0d3cc6f780a2af361fe1d927534ba31d2d86
konradotto <konrad-otto@gmx.de>
2019-09-20 01:25:24 +0200

    Rename increment to increment_ in timecontrolModel.

--- server/entities/timecontrol/timecontrolModel.js
+++ server/entities/timecontrol/timecontrolModel.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var timeControlSchema = new Schema({
     time: { type: Number, required: true },
-    increment: { type: Number, required: true }
+    increment_: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('timecontrols', timeControlSchema);
