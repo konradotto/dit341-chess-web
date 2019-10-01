5dbe9ac8b1339f0b1a81294da522c6b3aa645d73
konradotto <konrad-otto@gmx.de>
2019-10-01 18:07:02 +0200

    Store global application data.

---
+++ client/src/data/routes.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    href: 'login',
+    name: 'LogIn'
+  },
+  {
+    href: 'rankings',
+    name: 'Rankings'
+  },
+  {
+    href: 'games',
+    name: 'Games'
+  },
+  {
+    href: 'players',
+    name: 'Players'
+  }
+]
