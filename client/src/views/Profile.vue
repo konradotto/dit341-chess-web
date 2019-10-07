cf95cfd18d2c2a01f748f09a6591c2a0dc5ab4f7
bartekspitza <bartekspitza@hotmail.com>
2019-10-08 01:45:44 +0200

    added a basic profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="container-fluid">
-      <h1> profile page </h1>
+      <h1 class=profile-title> {{userName + '\' profile'}} </h1>
+      <p> {{'Username: ' + userName}} </p>
+      <p> {{'First name: ' + firstName}} </p>
+      <p> {{'Last name: ' + lastName}} </p>
+      <p> {{'email: ' + email}} </p>
   </div>
 </template>
 
 <script>
+import { Api } from '@/Api'
 export default {
-  components: {
-  },
   data() {
     return {
+      userName: '',
+      firstName: '',
+      lastName: '',
+      email: ''
     }
   },
-  methods: {
+  created() {
+    if (this.$store.state.userId) {
+      Api.get('/users/' + this.$store.state.userId)
+        .then((resp) => {
+          console.log(resp.data)
+          this.userName = resp.data.userName
+          this.firstName = resp.data.firstName
+          this.lastName = resp.data.lastName
+          this.email = resp.data.email
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert(err.response.data.message)
+        })
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.profile-title {
+    margin-bottom: 60px;
+}
 </style>
